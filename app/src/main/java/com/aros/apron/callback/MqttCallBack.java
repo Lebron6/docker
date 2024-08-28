c9b129321715c6c36303442dd2950e40f7333244
Lebron6 <577348350@qq.com>
2024-08-28 13:17:55 +0800

    修改拉高时返航导致的bug

--- app/src/main/java/com/aros/apron/callback/MqttCallBack.java
+++ app/src/main/java/com/aros/apron/callback/MqttCallBack.java
@@ -365,8 +365,8 @@ public class MqttCallBack implements MqttCallbackExtended {
                 break;
             //上传媒体文件
             case 60134:
-                LogUtil.log(TAG, "收到命令：获取里程" + jsonString);
-                FlightManager.getInstance().getAircraftTotalFlightDistance(mqttClient, message);
+//                LogUtil.log(TAG, "收到命令：获取里程" + jsonString);
+//                FlightManager.getInstance().getAircraftTotalFlightDistance(mqttClient, message);
                 break;
             //监听机库收到AMS命令后的回执
             case 60999:
