19e50ed89d296f2fb4478cec4c63fd7feaadb806
Lebron6 <577348350@qq.com>
2024-12-20 15:54:36 +0800

    修改降落

--- app/src/main/java/com/aros/apron/tools/ApronArucoDetect.java
+++ app/src/main/java/com/aros/apron/tools/ApronArucoDetect.java
@@ -541,10 +541,8 @@ public class ApronArucoDetect {
                 return 0.215;
             } else if (ultrasonicHeight > 0.5 && ultrasonicHeight <= 1.5) {
                 return 0.145;
-            } else if (ultrasonicHeight > 0.1 && ultrasonicHeight <= 0.5) {
-                return 0.085;
-            } else {
-                return 0.085;
+            } else  {
+                return 0.045;
             }
         } else if (d <= 500 && d > 400) {
             if (ultrasonicHeight > 9) {
@@ -553,10 +551,8 @@ public class ApronArucoDetect {
                 return 0.205;
             } else if (ultrasonicHeight > 0.5 && ultrasonicHeight <= 1.5) {
                 return 0.145;
-            } else if (ultrasonicHeight > 0.1 && ultrasonicHeight <= 0.5) {
-                return 0.085;
-            } else {
-                return 0.085;
+            } else  {
+                return 0.045;
             }
         } else if (d <= 400 && d > 300) {
             if (ultrasonicHeight > 9) {
@@ -565,10 +561,8 @@ public class ApronArucoDetect {
                 return 0.195;
             } else if (ultrasonicHeight > 0.5 && ultrasonicHeight <= 1.5) {
                 return 0.145;
-            } else if (ultrasonicHeight > 0.1 && ultrasonicHeight <= 0.5) {
-                return 0.085;
-            } else {
-                return 0.085;
+            } else  {
+                return 0.045;
             }
         } else if (d <= 300 && d > 250) {
             if (ultrasonicHeight > 9) {
@@ -577,10 +571,8 @@ public class ApronArucoDetect {
                 return 0.185;
             } else if (ultrasonicHeight > 0.5 && ultrasonicHeight <= 1.5) {
                 return 0.145;
-            } else if (ultrasonicHeight > 0.1 && ultrasonicHeight <= 0.5) {
-                return 0.085;
-            } else {
-                return 0.085;
+            }else  {
+                return 0.045;
             }
         } else if (d <= 250 && d > 200) {
             if (ultrasonicHeight > 9) {
@@ -589,10 +581,8 @@ public class ApronArucoDetect {
                 return 0.185;
             } else if (ultrasonicHeight > 0.5 && ultrasonicHeight <= 1.5) {
                 return 0.145;
-            } else if (ultrasonicHeight > 0.1 && ultrasonicHeight <= 0.5) {
-                return 0.085;
-            } else {
-                return 0.085;
+            } else  {
+                return 0.045;
             }
         }else if (d <= 200 && d > 150) {
             if (ultrasonicHeight > 9) {
@@ -601,10 +591,8 @@ public class ApronArucoDetect {
                 return 0.145;
             } else if (ultrasonicHeight > 0.5 && ultrasonicHeight <= 4) {
                 return 0.145;
-            } else if (ultrasonicHeight > 0.1 && ultrasonicHeight <= 0.5) {
-                return 0.085;
-            } else {
-                return 0.085;
+            } else  {
+                return 0.045;
             }
         } else if (d <= 150 && d > 100) {
             if (ultrasonicHeight > 9) {
@@ -613,21 +601,17 @@ public class ApronArucoDetect {
                 return 0.145;
             } else if (ultrasonicHeight > 0.5 && ultrasonicHeight <= 4) {
                 return 0.145;
-            } else if (ultrasonicHeight > 0.1 && ultrasonicHeight <= 0.5) {
-                return 0.085;
-            } else {
-                return 0.085;
+            }else  {
+                return 0.045;
             }
         } else if (d <= 100 && d > 79) {
             if (ultrasonicHeight > 9) {
                 return 0;
             } else if (ultrasonicHeight > 1.5 && ultrasonicHeight <= 9) {
-                return 0.085;
+                return 0.045;
             } else if (ultrasonicHeight > 0.5 && ultrasonicHeight <= 1.5) {
                 return 0.0;
-            } else if (ultrasonicHeight > 0.1 && ultrasonicHeight <= 0.5) {
-                return 0.0;
-            } else {
+            } else  {
                 return 0.0;
             }
         } else {
