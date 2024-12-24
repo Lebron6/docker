216bbd7a3db48b686dba15b4ff705e0d3d403612
Lebron6 <577348350@qq.com>
2024-12-24 09:14:20 +0800

    修改9米以下摇杆数据固定

--- app/src/main/java/com/aros/apron/tools/ApronArucoDetect.java
+++ app/src/main/java/com/aros/apron/tools/ApronArucoDetect.java
@@ -656,91 +656,91 @@ public class ApronArucoDetect {
             if (ultrasonicHeight > 9) {
                 return 0.325;
             } else if (ultrasonicHeight > 1.5 && ultrasonicHeight <= 9) {
-                return 0.145;
+                return 0.125;
             } else if (ultrasonicHeight > 0.5 && ultrasonicHeight <= 1.5) {
-                return 0.145;
+                return 0.125;
             } else if (ultrasonicHeight > 0.1 && ultrasonicHeight <= 0.5) {
-                return 0.145;
+                return 0.125;
             } else {
-                return 0.145;
+                return 0.125;
             }
         } else if (d <= 500 && d > 400) {
             if (ultrasonicHeight > 9) {
                 return 0.325;
             } else if (ultrasonicHeight > 1.5 && ultrasonicHeight <= 9) {
-                return 0.145;
+                return 0.125;
             } else if (ultrasonicHeight > 0.5 && ultrasonicHeight <= 1.5) {
-                return 0.145;
+                return 0.125;
             } else if (ultrasonicHeight > 0.1 && ultrasonicHeight <= 0.5) {
-                return 0.145;
+                return 0.125;
             } else {
-                return 0.145;
+                return 0.125;
             }
         } else if (d <= 400 && d > 300) {
             if (ultrasonicHeight > 9) {
                 return 0.325;
             } else if (ultrasonicHeight > 1.5 && ultrasonicHeight <= 9) {
-                return 0.145;
+                return 0.125;
             } else if (ultrasonicHeight > 0.5 && ultrasonicHeight <= 1.5) {
-                return 0.145;
+                return 0.125;
             } else if (ultrasonicHeight > 0.1 && ultrasonicHeight <= 0.5) {
-                return 0.145;
+                return 0.125;
             } else {
-                return 0.145;
+                return 0.125;
             }
         } else if (d <= 300 && d > 250) {
             if (ultrasonicHeight > 9) {
                 return 0.295;
             } else if (ultrasonicHeight > 1.5 && ultrasonicHeight <= 9) {
-                return 0.185;
+                return 0.125;
             } else if (ultrasonicHeight > 0.5 && ultrasonicHeight <= 1.5) {
-                return 0.145;
+                return 0.125;
             } else if (ultrasonicHeight > 0.1 && ultrasonicHeight <= 0.5) {
-                return 0.145;
+                return 0.125;
             } else {
-                return 0.145;
+                return 0.125;
             }
         } else if (d <= 250 && d > 200) {
             if (ultrasonicHeight > 9) {
                 return 0.295;
             } else if (ultrasonicHeight > 1.5 && ultrasonicHeight <= 9) {
-                return 0.145;
+                return 0.125;
             } else if (ultrasonicHeight > 0.5 && ultrasonicHeight <= 1.5) {
-                return 0.145;
+                return 0.125;
             } else if (ultrasonicHeight > 0.1 && ultrasonicHeight <= 0.5) {
-                return 0.145;
+                return 0.125;
             } else {
-                return 0.145;
+                return 0.125;
             }
         }else if (d <= 200 && d > 150) {
             if (ultrasonicHeight > 9) {
                 return 0.275;
             } else if (ultrasonicHeight > 4 && ultrasonicHeight <= 9) {
-                return 0.145;
+                return 0.125;
             } else if (ultrasonicHeight > 0.5 && ultrasonicHeight <= 4) {
-                return 0.145;
+                return 0.125;
             } else if (ultrasonicHeight > 0.1 && ultrasonicHeight <= 0.5) {
-                return 0.145;
+                return 0.125;
             } else {
-                return 0.145;
+                return 0.125;
             }
         } else if (d <= 150 && d > 100) {
             if (ultrasonicHeight > 9) {
-                return 0.175;
-            } else if (ultrasonicHeight > 4 && ultrasonicHeight <= 9) {
-                return 0.145;
-            } else if (ultrasonicHeight > 0.5 && ultrasonicHeight <= 4) {
-                return 0.145;
-            } else if (ultrasonicHeight > 0.1 && ultrasonicHeight <= 0.5) {
-                return 0.145;
+                return 0.125;
+//            } else if (ultrasonicHeight > 4 && ultrasonicHeight <= 9) {
+//                return 0.125;
+//            } else if (ultrasonicHeight > 0.5 && ultrasonicHeight <= 4) {
+//                return 0.125;
+//            } else if (ultrasonicHeight > 0.1 && ultrasonicHeight <= 0.5) {
+//                return 0.125;
             } else {
-                return 0.145;
+                return 0.125;
             }
         } else if (d <= 100 && d > 79) {
             if (ultrasonicHeight > 9) {
                 return 0;
             } else if (ultrasonicHeight > 1.5 && ultrasonicHeight <= 9) {
-                return 0.145;
+                return 0.125;
             } else if (ultrasonicHeight > 0.5 && ultrasonicHeight <= 1.5) {
                 return 0.0;
             } else if (ultrasonicHeight > 0.1 && ultrasonicHeight <= 0.5) {
