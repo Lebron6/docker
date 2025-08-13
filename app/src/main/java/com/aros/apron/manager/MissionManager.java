a099b0d7cb03553be79399850148c5efb70ef20d
Lebron6 <577348350@qq.com>
2025-08-13 11:13:37 +0800

    增加无人机自检的时间

--- app/src/main/java/com/aros/apron/manager/MissionManager.java
+++ app/src/main/java/com/aros/apron/manager/MissionManager.java
@@ -394,7 +394,7 @@ public class MissionManager extends BaseManager {
 
     //等待航线任务状态更新或RTK健康状态刷新
     private void verifyAircraftStatus(MqttAndroidClient client, MQMessage message) {
-        if (checkMissionStateTimes < 50) {
+        if (checkMissionStateTimes < 100) {
             mainHandler.postDelayed(new Runnable() {
                 @Override
                 public void run() {
