804fbde8d2ab5adebbf98ad157f10f92a31d65b2
Lebron6 <577348350@qq.com>
2025-09-09 16:06:53 +0800

    修改M400代码

--- app/src/main/java/com/aros/apron/activity/ConnectionActivity.kt
+++ app/src/main/java/com/aros/apron/activity/ConnectionActivity.kt
@@ -330,25 +330,25 @@ open class ConnectionActivity : BaseActivity() {
             .start()
     }
     private fun toMain(){
-//        val isFlightControllerConnect =
-//            KeyManager.getInstance().getValue(DJIKey.create(FlightControllerKey.KeyConnection))
-//        val cameraType = KeyManager.getInstance().getValue(
-//            KeyTools.createKey(
-//                CameraKey.KeyCameraType,
-//                ComponentIndexType.PORT_1
-//            )
-//        )
-//        if (isFlightControllerConnect == null || !isFlightControllerConnect||cameraType==CameraType.NOT_SUPPORTED) {
-//            handler.postDelayed({
-//                toMain()
-//            }, 1000)
-//        } else {
-//            checkTimes++
-//            LogUtil.log(TAG, "飞机是否连接$checkTimes${cameraType?.name}")
+        val isFlightControllerConnect =
+            KeyManager.getInstance().getValue(DJIKey.create(FlightControllerKey.KeyConnection))
+        val cameraType = KeyManager.getInstance().getValue(
+            KeyTools.createKey(
+                CameraKey.KeyCameraType,
+                ComponentIndexType.PORT_1
+            )
+        )
+        if (isFlightControllerConnect == null || !isFlightControllerConnect||cameraType==CameraType.NOT_SUPPORTED) {
+            handler.postDelayed({
+                toMain()
+            }, 1000)
+        } else {
+            checkTimes++
+            LogUtil.log(TAG, "飞机是否连接$checkTimes${cameraType?.name}")
             if (!MainActivity.isAppStarted) {
                     startActivity(Intent(this, MainActivity::class.java))
             }
-//        }
+        }
     }
 
     private fun <T> enableShowCaseButton(view: View, cl: Class<T>) {
