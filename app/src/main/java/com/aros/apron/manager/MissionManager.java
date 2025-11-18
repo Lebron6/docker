2b7736935a9a9add49fc0dc9270454bab1df0ee3
Lebron6 <577348350@qq.com>
2025-11-18 10:53:02 +0800

    航线状态判断条件修改

--- app/src/main/java/com/aros/apron/manager/MissionManager.java
+++ app/src/main/java/com/aros/apron/manager/MissionManager.java
@@ -451,7 +451,7 @@ public class MissionManager extends BaseManager {
         }
         if (PreferenceUtils.getInstance().getHaveRTK()) {
             if (
-                    (missionStateCode == 2) &&
+                    (missionStateCode == 2 || missionStateCode == 0)&&
 
                     Movement.getInstance().isRtkSign() &&
                     (!TextUtils.isEmpty(Movement.getInstance().getPlaneMessage()) && !Movement.getInstance().getPlaneMessage().equals("无法起飞"))) {
@@ -463,7 +463,7 @@ public class MissionManager extends BaseManager {
             }
         } else {
             //没有RTK的情况下延迟下载航线，等待GPS信号收敛
-            if ((missionStateCode == 2) &&
+            if ((missionStateCode == 2 || missionStateCode == 0) &&
                     (!TextUtils.isEmpty(Movement.getInstance().getPlaneMessage())
                             && !Movement.getInstance().getPlaneMessage().equals("无法起飞")
                             && (Movement.getInstance().getGPSSignalLevel().equals("LEVEL_4")
@@ -519,7 +519,7 @@ public class MissionManager extends BaseManager {
                     if (!Movement.getInstance().isRtkSign()) {
                         LogUtil.log(TAG, "飞行器RTK收敛异常");
                         sendMissionExecuteEvents( "飞行器RTK收敛异常");
-                    } else if (!(missionStateCode == 2)) {
+                    } else if (!(missionStateCode ==2 || missionStateCode == 0)) {
                     LogUtil.log(TAG, "飞行器航线状态异常:" + WaypointMissionExecuteState.find(missionStateCode).name());
                         sendMissionExecuteEvents( "飞行器航线状态异常:" + WaypointMissionExecuteState.find(missionStateCode).name());
                     } else {
